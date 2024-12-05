716800017786c533325fcbb0bc266929ab90c1cb
anouar garbaa <g.anouar@yahoo.com>
2024-12-05 15:15:02 +0100

    adding NewPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Anouar" body="I mastered ReactJS" />
-      <Post author="Max" body="I am learning ReactJS" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Anouar" body="I mastered ReactJS" />
+        <Post author="Max" body="I am learning ReactJS" />
+      </ul>
+    </>
   );
 }
 
