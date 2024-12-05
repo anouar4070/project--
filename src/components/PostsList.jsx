627ad9c3f9fa1a25e088191e7a42cf4b64415e11
anouar garbaa <g.anouar@yahoo.com>
2024-12-05 18:46:09 +0100

    handling form submission

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,31 +6,15 @@ import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
 
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Max" body="I am learning ReactJS" />
       </ul>
     </>
