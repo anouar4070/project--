9daa25d56eb3da138e4a6579f65d9439dc28793f
anouar garbaa <g.anouar@yahoo.com>
2024-12-05 23:11:42 +0100

    add Layout Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App'
-import NewPost from './components/NewPost'
-import './index.css'
-
+import App from "./App";
+import NewPost from "./components/NewPost";
+import RootLayout from "./routes/RootLayout";
+import "./index.css";
 
 const router = createBrowserRouter([
-  {path: '/', element: <App />},
-  {path: '/create-post', element: <NewPost />},
-])
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router ={router} />
-    
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
